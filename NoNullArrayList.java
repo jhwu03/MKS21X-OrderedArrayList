a8bacdb344f4f58aae027da86f46157aaa2a102c
Jionghao <jwu03@stuy.edu>
2018-11-01 11:48:11 -0400

    start on NoNull

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -3,6 +3,9 @@ import java.util.List;
 public class NoNullArrayList<T> extends ArrayList<T>{
 
   public void add(int index, T value){
-    
+    if (value == null){
+      throw new IllegalArgumentException ("can't be null");
+  }else{
+    Super.add(index,value);
   }
 }
