546983e4a0fcbd2b9383a431d0efaf3914b0be5d
Jionghao <jwu03@stuy.edu>
2018-11-02 11:25:49 -0400

    updated add and set methods,with constructors

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,11 +1,34 @@
 import java.util.ArrayList;
 import java.util.List;
 public class NoNullArrayList<T> extends ArrayList<T>{
+  public NoNullArrayList(){
+    super();
+  }
+  public NoNullArrayList(int cap){
+    super(cap);
+  }
+  public boolean add(T value){
+    if (value == null){
+      throw new IllegalArgumentException ("can't be null");
+  }else{
+    super.add(value);
+  }
+  return true;
+}
 
   public void add(int index, T value){
     if (value == null){
       throw new IllegalArgumentException ("can't be null");
   }else{
-    Super.add(index,value);
+    super.add(index,value);
   }
+}
+
+  public T set(int index, T value){
+    if (value == null){
+      throw new IllegalArgumentException ("can't be null");
+  }
+    return super.set(index,value);
+}
+
 }
