cf5e3a2581047dc7b3d0d0fddfc270d627bdcc8c
jwu03 <jwu03@stuy.edu>
2018-11-04 22:31:47 -0500

    added helper method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,6 +8,19 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(cap);
   }
 
+  private int Index(T value){
+    boolean right = false ;
+    int index = 0 ;
+    while ( bad == false && index < this.size() ) {
+      if ( value.compareTo( this.get(index) ) <= 0 ) {
+        bad = true ;
+      }else{
+        index = index + 1 ;
+      }
+    }
+    return index ;
+  }
+
   public boolean add(T value){
     int i = 0;
     if (value.compareTo(this.get(i)) > 0){
