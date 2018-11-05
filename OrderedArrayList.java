e57a75e3e46c06e2f598ccfedd579b5c4a7ccaa4
jwu03 <jwu03@stuy.edu>
2018-11-04 22:48:33 -0500

    finished OrderedArrayList

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -11,9 +11,9 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   private int Index(T value){
     boolean right = false ;
     int index = 0 ;
-    while ( bad == false && index < this.size() ) {
+    while ( right == false && index < this.size() ) {
       if ( value.compareTo( this.get(index) ) <= 0 ) {
-        bad = true ;
+        right = true ;
       }else{
         index = index + 1 ;
       }
@@ -23,15 +23,27 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
 
   public boolean add(T value){
     int i = 0;
-    if (value.compareTo(this.get(i)) > 0){
-      i = i + 1;
-      add(value);
-    }
-    if(value.compareTo(this.get(i)) <= 0){
-      super.add(i,value);
-      i = 0;
-    }
+    if (value == null){
+  throw new IllegalArgumentException ();
+}else{
+  super.add(Index(value), value);
+}
     return true;
   }
+
+  public void add (int index, T value){
+    this.add(value);
+  }
+
+  public T set (int index, T value){
+    if (value == null){
+      throw new IllegalArgumentException ();
+    }else{
+      T nn = this.get(index);
+      this.remove(index);
+      add(value);
+      return nn;
+      }
+    }
 
 }
